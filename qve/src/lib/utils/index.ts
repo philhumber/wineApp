f8efe6a3d876a7c5b8f7a748b694b07e1192e943
Phil Humber <phil.humber@gmail.com>
2026-01-31 20:00:55 +0100

    added natural text capability for key commands (start, cancel, end, etc)

---
+++ qve/src/lib/utils/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Utilities Module
+ * Pure helper functions used across the application
+ */
+
+// Command Detection
+export { detectCommand, type CommandType, type CommandDetectionResult } from './commandDetector';
